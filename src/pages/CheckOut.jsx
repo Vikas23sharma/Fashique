e67381d326ff63271da4652200c8897846ea851c
Shanky43 <shankarshanky43@gmail.com;git config --global user.name Shanky43git config --global user.email shankarshanky43@gmail.com>
2023-03-31 11:49:59 +0530

    day5

--- src/pages/CheckOut.jsx
+++ src/pages/CheckOut.jsx
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 import CheckoutProductsCard from './CheckoutProductsCard';
 import LoadingWithLetter from './LoadingWithLetter';
 import "../Style/checkout.css"
+import { AiOutlineInfoCircle } from 'react-icons/ai';
 
 const CheckOut = () => {
     const [textChange, setTextChange] = useState("Change")
@@ -217,7 +218,7 @@ const CheckOut = () => {
     return (
         <Container maxW={"100%"}>
             <Center>
-                <Box width={["100%", "80%", "80%", "60%",]} p="5">
+                <Box width={["100%", "80%", "80%", "90%", "60%",]} p="5">
                     <HStack>
                         <Box><Text fontSize={["md", "3xl"]} fontWeight="bolder">FASHIQUE</Text></Box>
                         <Spacer />
@@ -228,13 +229,14 @@ const CheckOut = () => {
                 </Box>
             </Center>
             <Center>
-                <Box width={["700px", "100%", "80%", "60%"]} p="5">
+                <Box width={["700px", "100%", "80%", "100%", "60%"]} p="5">
+
                     <Stack direction={["column", "column", "column", "row"]}>
                         <Box style={{ boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px", }}
-                            w={["100%", "100%", "100%", "800px"]} maxH={["17vh", "17vh", "17vh", "17vh"]} mt="0"
+                            w={["100%", "100%", "100%", "1600px", "800px"]} maxH={["17vh", "17vh", "25vh", "20vh", "17vh"]} mt="0"
                         >
                             <Box pl={["5", "10"]} pt="5" pb={"10"}>
-                                <Text fontSize={["md", "xl"]} fontWeight="520">DELIVERY COUNTRY :</Text>
+                                <Text fontSize={["md", "xl"]} fontWeight="550">DELIVERY COUNTRY :</Text>
                                 <HStack>
                                     <Box width={"40px"} pt="5">
                                         <HStack width={"100%"} >
@@ -263,16 +265,30 @@ const CheckOut = () => {
                                     </Box>
                                 </HStack>
                             </Box>
+                            <Box border={"1px solid black"} h={["100vh", "100vh", "50vh", "50vh", "10vh"]} mt={["55vh", "58vh", "65vh", "5vh", "0vh"]}>
+                                <Box>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, laudantium molestias ab quasi consequuntur eum illo facilis eos voluptatum quae corporis veritatis. Nostrum commodi facilis fugiat repellendus esse recusandae expedita?
+                                    Assumenda temporibus sed similique inventore. Accusamus voluptatem provident officia exercitationem nemo, ut et at magnam, reprehenderit possimus tempora necessitatibus fugiat! Perspiciatis reprehenderit in, quam at eum facere nulla laboriosam veritatis.
+                                    Eum laborum eaque, necessitatibus dolorum illo assumenda, minus nesciunt doloremque consequatur ipsa, neque doloribus veniam praesentium impedit reiciendis maiores voluptatum? Libero, quisquam iure! Recusandae modi magnam voluptatum excepturi temporibus animi.
+                                    Ad, error consequatur culpa minus fuga odio quod veniam vero voluptates fugit unde architecto corporis laborum, quam doloremque, harum voluptatibus aliquid. Dolor accusamus quos vero aut maiores illum non cumque.
+                                    Animi quisquam dicta quaerat ad labore harum, sequi nemo, reprehenderit amet corrupti incidunt quasi debitis. Earum recusandae tenetur error est, ipsum facilis deserunt, dignissimos blanditiis, obcaecati nostrum eos vero iste?
+                                    Necessitatibus, quidem quas unde facere nostrum ab rerum deleniti at magni, amet distinctio impedit quisquam fugiat numquam aperiam hic placeat consequatur sit provident harum. Officia eaque voluptatem doloribus! Quia, consequatur.
+                                    Labore iure qui ratione dolore commodi eligendi at quae illum ut dolorum non odio vitae eaque quibusdam, corporis accusamus laborum architecto sapiente atque distinctio doloremque voluptatibus saepe voluptatum? Veniam, atque.
+                                    Architecto possimus nam sapiente libero, voluptas amet sint officia ipsa et cum alias quisquam animi! Alias, ducimus nemo magni vero voluptatum nam iste deleniti et quo quae asperiores maiores aspernatur.
+                                    Magni soluta enim minus iusto quis aliquam eos officia numquam quidem, ex voluptatum repellat eligendi cumque, repudiandae aspernatur accusantium corrupti consequuntur eius blanditiis. Fugit dolorem rem asperiores vitae quasi nesciunt!
+                                    Dicta temporibus adipisci repellat velit maxime ad commodi sunt praesentium, architecto sed debitis eligendi quibusdam ipsam itaque id accusantium modi reprehenderit eum, autem magni odio eos nobis esse facilis? Natus.</Box>
+                            </Box>
                         </Box>
+
                         <Box style={{ boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px", }}
-                            w={["100%", "100%", "100%", "400px"]}
+                            w={["100%", "100%", "100%", "100%", "400px"]}
                             minH={["10vh", "10vh", "10vh", "10vh"]}
                             margin={["auto", "auto", "auto", "auto"]}
+                            marginTop={["50vh", "50vh", "50vh", "50vh", "10vh"]}
                         >
                             <HStack>
                                 <Box p="5">
                                     <Text letterSpacing={2} fontWeight="600">
-                                        {isMobileView ? <Text fontSize={["md", "xl", "xl", "xl"]}>MY BAG</Text> : `${checkoutProducts.length} ITEMS`}
+                                        {isMobileView ? <Text fontSize={["md", "xl", "xl", "xl", "xl"]}>MY BAG</Text> : `${checkoutProducts.length} ITEMS`}
                                     </Text>
                                 </Box>
                                 <Spacer />
@@ -286,7 +302,7 @@ const CheckOut = () => {
                             </HStack>
                             <Divider width={"90%"} margin="auto" />
                             <Box  >
-                                <Box h="40vh"
+                                <Box h="30vh"
                                     overflowY="scroll"
                                     className='hide-scrollbar' mt="5"
                                     css={{ scrollBehavior: 'smooth' }}
@@ -305,32 +321,40 @@ const CheckOut = () => {
                                             <Spacer />
                                             <Box><Text pr={"10"} pt="5" fontSize={["md", "xl", "xl", "xl"]}>&#36;{totalPrice}.00</Text></Box>
                                         </HStack>
-                                    </Box> : <Box p="2" h="20vh">
+                                    </Box> : <Box h={["30vh", "50vh", "20vh", "20vh", "25vh"]}>
                                         <Divider orientation="horizontal" height="2px" bg="gray.200" my={4} width={"90%"} margin="auto" />
                                         <VStack pl={5} pr={5} pt="5">
                                             <Flex justifyContent="space-between" width="100%">
                                                 <Text>SubTotal</Text>
-                                                <Text>&#36;{totalPrice}.00</Text>
+                                                <Text>&#36; {totalPrice}.00</Text>
                                             </Flex>
                                             <Flex justifyContent="space-between" width="100%">
                                                 <Text>Delivery</Text>
-                                                <Text>&#36;{deliveryPrice}.00</Text>
+                                                <Text>&#36; {deliveryPrice}.00</Text>
                                             </Flex>
                                             <Flex justifyContent="space-between" width="100%">
                                                 <Text fontWeight={"bolder"}>TOTAL TO PAY</Text>
-                                                <Text fontWeight={"bolder"}>&#36;{totalToPay}.00</Text>
+                                                <Text fontWeight={"bolder"}>&#36;   {totalToPay}</Text>
                                             </Flex>
+                                            {!isMobileView && <Box bg={"#cde2f5"} borderRadius={"5"} p={["20px", "0px"]}
+                                                display={["block", "block", "none", "none", "block"]}>
+                                                <Flex justifyContent={"space-between"} pt="2" pb="2" >
+                                                    <Text pt={"4"} pl="3" ><AiOutlineInfoCircle fontWeight={"900"} fontSize={"20px"} /></Text>
+                                                    <Spacer />
+                                                    <Text fontSize={"12"} textAlign={"left"} pl="3" fontWeight={"900"}>FREE STANDARD DELIVERY TO INDIA WHEN YOU SPEND OVER ${totalToPay + 4}</Text>
+                                                </Flex>
+                                            </Box>}
                                         </VStack>
-
-
-
-
                                     </Box>
                                 }
                             </Box>
 
                         </Box>
+
                     </Stack>
+                    {/* <Box border={"1px solid black"} h="10">
+
+                    </Box> */}
                 </Box>
             </Center >
         </Container >
