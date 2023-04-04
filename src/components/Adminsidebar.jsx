fa6fc1c5a64af74981bd1022b652a79f94796943
Vikas23sharma <112754659+Vikas23sharma@users.noreply.github.com>
2023-04-04 09:17:11 +0530

    full responsive on mobile screen as well

--- src/components/Adminsidebar.jsx
+++ src/components/Adminsidebar.jsx
@@ -21,81 +21,85 @@ import "../Style/Admindashboard.css"
 const Adminsidebar = () => {
     return (
         <Box className='sidebar' backgroundColor={"#2d2d2d"} >
-            <Flex className='adminsidebar' justifyContent={"space-evenly"} >
+            <Flex className='adminsidebar'  >
+
                 <Link to={"/admin"}>
-                    <Flex  justifyContent={"space-evenly"} padding={"6%"} mt={"1%"}  >
+                    <Flex className='three' justifyContent={"space-evenly"} padding={"6%"}   >
                         <Box className='iconcont' ><AiOutlineHome className='icons' color='white' size={27} /></Box>
                         <Box className='labelcont'> <Text className='labels' color='white' >Home</Text></Box>
                     </Flex>
                 </Link>
                 <Link to={"/adminorders"}>
-                    <Flex  justifyContent={"space-evenly"} padding={"6%"} >
+                    <Flex className='three' justifyContent={"space-evenly"} padding={"6%"} >
                         <Box className='iconcont'  ><AiOutlineShoppingCart className='icons' color='white' size={27} /></Box>
                         <Box className='labelcont'><Text className='labels' color='white' >Orders</Text></Box>
                     </Flex>
                 </Link>
                 <Link to="/admincustomers">
-                    <Flex  justifyContent={"space-evenly"} padding={"4%"} >
+                    <Flex className='three' justifyContent={"space-evenly"} padding={"4%"} >
                         <Box className='iconcont' ><FiUsers className='icons' color='white' size={25} /></Box>
-                        <Box className='labelcont'  id='usercontainer'  ><Text className='labels' color='white' >Users</Text></Box>
+                        <Box className='labelcont' id='usercontainer'  ><Text className='labels' color='white' >Users</Text></Box>
                     </Flex>
                 </Link>
 
-                <Accordion className='sidebarchild' border={"0px solid #2d2d2d "} defaultIndex={[1]} allowMultiple>
-                    <AccordionItem padding={"3%"}>
-                        <h2>
-                            <AccordionButton color={"white"}>
-                                <Flex justifyContent="space-evenly" as="span" flex='1' textAlign='center'>
-                                    <HiOutlineClipboardList id='productsicon' className='icons' color='white' size={27} />
-                                    <Text className='labels' color='white' >Products</Text>
-                                </Flex>
-                                <AccordionIcon />
-                            </AccordionButton>
-                        </h2>
-                        <AccordionPanel pb={4} textAlign="center">
-                            <Link to={"/adminproductmen"}>
-                                <Text className='sections' color='white' boxShadow="rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px" padding="2%" >MEN</Text>
-                            </Link>
+                
+                    <Accordion className='sidebarchild1' border={"0px solid #2d2d2d "} defaultIndex={[1]} allowMultiple>
+                        <AccordionItem padding={"3%"}>
+                            <h2>
+                                <AccordionButton className='accordionbutton' color={"white"}>
+                                    <Flex justifyContent="space-evenly" as="span" flex='1' textAlign='center'>
+                                        <HiOutlineClipboardList id='productsicon' className='icons' color='white' size={27} />
+                                        <Text className='labels' color='white' >Products</Text>
+                                    </Flex>
+                                    <AccordionIcon />
+                                </AccordionButton>
+                            </h2>
+                            <AccordionPanel pb={4} textAlign="center">
+                                <Link to={"/adminproductmen"}>
+                                    <Text className='sections' color='white' boxShadow="rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px" padding="2%" >MEN</Text>
+                                </Link>
 
-                            <Link to={"/admin-product-women"}>
+                                <Link to={"/admin-product-women"}>
+                                    <Text className='sections' color='white' boxShadow="rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px" padding="2%" >WOMEN</Text>
+                                </Link>
+
+                                <Link to={"/admin-product-shoes"}>
+                                    <Text className='sections' color='white' boxShadow="rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px" padding="2%" >SHOES</Text>
+                                </Link>
+
+
+                                <Link to={"/admin-product-jacket"}>
+                                    <Text className='sections' color='white' boxShadow="rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px" padding="2%" >JACKETS</Text>
+                                </Link>
+
+                            </AccordionPanel>
+                        </AccordionItem>
+                    </Accordion>
+                    <Accordion className='sidebarchild2' border={"0px solid #2d2d2d "} defaultIndex={[1]} allowMultiple >
+                        <AccordionItem padding={"3%"}>
+                            <h2>
+                                <AccordionButton color={"white"}>
+                                    <Flex display={"flex"} justifyContent="space-evenly" as="span" flex='1' textAlign='center'>
+                                        <BsPlusCircle className='icons' color='white' size={25} />
+                                        <Text className='labels' color='white' >Add <br /> Products</Text>
+                                    </Flex>
+                                    <AccordionIcon />
+                                </AccordionButton>
+                            </h2>
+                            <AccordionPanel textAlign={"center"} pb={4}>
+                                <Link to={"/adminaddproductmen"}>
+                                    <Text className='sections' color='white' boxShadow="rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px" padding="2%" >MEN</Text>
+                                </Link>
                                 <Text className='sections' color='white' boxShadow="rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px" padding="2%" >WOMEN</Text>
-                            </Link>
-
-                            <Link to={"/admin-product-shoes"}>
                                 <Text className='sections' color='white' boxShadow="rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px" padding="2%" >SHOES</Text>
-                            </Link>
-
-
-                            <Link to={"/admin-product-jacket"}>
                                 <Text className='sections' color='white' boxShadow="rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px" padding="2%" >JACKETS</Text>
-                            </Link>
+                            </AccordionPanel>
+                        </AccordionItem>
+                    </Accordion>
 
-                        </AccordionPanel>
-                    </AccordionItem>
-                </Accordion>
-                <Accordion className='sidebarchild' border={"0px solid #2d2d2d "} defaultIndex={[1]} allowMultiple >
-                <AccordionItem padding={"3%"}>
-                        <h2>
-                            <AccordionButton color={"white"}>
-                                <Flex display={"flex"} justifyContent="space-evenly" as="span" flex='1' textAlign='center'>
-                                    <BsPlusCircle className='icons' color='white' size={25} />
-                                    <Text className='labels'  color='white' >Add <br/> Products</Text>
-                                </Flex>
-                                <AccordionIcon />
-                            </AccordionButton>
-                        </h2>
-                        <AccordionPanel textAlign={"center"} pb={4}>
-                            <Link to={"/adminaddproductmen"}>
-                            <Text className='sections' color='white' boxShadow="rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px" padding="2%" >MEN</Text>
-                            </Link>
-                            <Text className='sections' color='white' boxShadow="rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px" padding="2%" >WOMEN</Text>
-                            <Text className='sections' color='white' boxShadow="rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px" padding="2%" >SHOES</Text>
-                            <Text className='sections' color='white' boxShadow="rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px" padding="2%" >JACKETS</Text>
-                        </AccordionPanel>
-                    </AccordionItem>
-                </Accordion>
+               
 
-                <Flex></Flex>
+
             </Flex>
         </Box>
     )
